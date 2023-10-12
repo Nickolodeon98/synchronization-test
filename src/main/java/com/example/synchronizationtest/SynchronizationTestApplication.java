a56301314e2d6131a87af74fac4015a8dd4556a0
Nickolodeon98 <sjeon0730@gmail.com>
2023-10-12 20:22:18 +0900

    feat: add a project

---
+++ src/main/java/com/example/synchronizationtest/SynchronizationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.synchronizationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronizationTestApplication.class, args);
+	}
+
+}
